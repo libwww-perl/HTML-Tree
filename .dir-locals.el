00eee451e704cc722448534876c01e5fe173f1de
Jeff Fearn <jfearn@redhat.com>
2017-04-18 12:15:13 +1000

    RT #94311: Patch: as_XML() inserts extra whitespace in comments
    Patch from tomaz.solc

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-;; Local variables for Emacs
-
-((cperl-mode . ((cperl-indent-level . 4))))
--- lib/HTML/Element.pm
+++ lib/HTML/Element.pm
@@ -2325,7 +2325,7 @@ sub starttag_XML {
             $name = $self->{'text'};
         }
         $name =~ s/--/-&#45;/g;    # can't have double --'s in XML comments
-        return "<!-- $name -->";
+        return "<!--$name-->";
     }
 
     my $tag = "<$name";
